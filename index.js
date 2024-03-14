a7ca2055e286e9d84d155d647ad73447c1a1cf99
LeoFernandez037 <leonelfernandezuwu@gmail.com>
2024-03-14 09:17:25 -0400

    terminado puesss

--- index.js
+++ index.js
@@ -1,21 +1,74 @@
-console.log("Hola mundo");
-function Suma(a, b) {
-  let c = a + b;
-  // debugger;
-  return c;
-}
-// averiguar de scope porque var no se utiliza solo let y const
+const createTarjeta = () => {
+  const imagen = document.createElement("img");
+  imagen.className = "redonda";
+  imagen.src = "./ProyectoPresentacion/avatar-jessica.jpeg";
+  const contImg = document.createElement("div");
+  contImg.className = "contenedorImagen";
 
-let botoncito = document.querySelector("button");
-botoncito.name = "maicra";
+  const name = document.createElement("h1");
+  name.className = "nombre";
+  const nameText = document.createTextNode("Lorem ipsum");
 
-// para elegir mas botones selecionamos lo siguiente
+  const ubication = document.createElement("p");
+  ubication.className = "ubicacion";
+  const ubicationText = document.createTextNode("La Paz Bolivia");
 
-//let boton = document.querySelector("button");
+  const description = document.createElement("p");
+  description.className = "descripcion";
+  const text = document.createTextNode(
+    '"Lorem ipsum dolor sit amet vel voluptas."'
+  );
 
-let facebook = document.createElement("button");
-let facebookTexto = document.createTextNode("Facebook");
-facebook.appendChild(facebookTexto);
-let contenedor = document.querySelector(".container");
-//. porque es el nombre de una clase
-contenedor.appendChild(facebook);
+  const cont1 = document.createElement("div");
+  cont1.className = "Contenedor1";
+  name.appendChild(nameText);
+  cont1.appendChild(name);
+  ubication.appendChild(ubicationText);
+  cont1.appendChild(ubication);
+  description.appendChild(text);
+  cont1.appendChild(description);
+
+  const buttonGit = document.createElement("button");
+  const buttonGitTexto = document.createTextNode("GitHub");
+  buttonGit.appendChild(buttonGitTexto);
+
+  const buttonFront = document.createElement("button");
+  const buttonFrontTexto = document.createTextNode("Front Mentor");
+  buttonFront.appendChild(buttonFrontTexto);
+
+  const buttonInsta = document.createElement("button");
+  const buttonInstaTexto = document.createTextNode("Instagram");
+  buttonInsta.appendChild(buttonInstaTexto);
+
+  const buttonLink = document.createElement("button");
+  const buttonLinkTexto = document.createTextNode("Linkedln");
+  buttonLink.appendChild(buttonLinkTexto);
+
+  const buttonTwitter = document.createElement("button");
+  const buttonTwitterTexto = document.createTextNode("Twitter");
+  buttonTwitter.appendChild(buttonTwitterTexto);
+
+  const contButtons = document.createElement("div");
+  contButtons.className = "container";
+  contButtons.appendChild(buttonInsta);
+  contButtons.appendChild(buttonTwitter);
+  contButtons.appendChild(buttonLink);
+  contButtons.appendChild(buttonFront);
+  contButtons.appendChild(buttonGit);
+
+  const profileCard = document.createElement("div");
+  profileCard.className = "contenedorDinamico";
+  contImg.appendChild(imagen);
+  profileCard.appendChild(contImg);
+  cont1.appendChild(contButtons);
+  profileCard.appendChild(cont1);
+  return profileCard;
+};
+
+const mountNode = document.querySelector("#app");
+const addCard = () => {
+  mountNode.appendChild(createTarjeta());
+};
+
+const addCardButton = document.querySelector("button");
+addCardButton.addEventListener("click", addCard);
